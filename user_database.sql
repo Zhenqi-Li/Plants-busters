a8654cb39ffc98020749676f28c15afb8f347bbe
walterscorpionblade <36717405+walterscorpionblade@users.noreply.github.com>
2018-04-07 16:16:17 -0600

    Add files via upload

---
+++ user_database.sql
@@ -0,0 +1,12 @@
+drop table user_database;
+
+create table user_database
+(
+	Emailaddress varchar(100) not null,
+    PassWord        varchar(100) not null
+    
+);
+
+insert into user_database(Emailaddress,PassWord) value ('zili5742@colorado.edu',123456);
+insert into user_database(Emailaddress,PassWord) value ('jiwa8909@colorado.edu',111111);
+insert into user_database(Emailaddress,PassWord) value ('hali3456@colorado.edu',121212);
